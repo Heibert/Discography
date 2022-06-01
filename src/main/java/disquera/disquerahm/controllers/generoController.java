41e931ad1e3d8e2be41d5a9c6880fe76275ef334
Heibert <hsmogollon8@misena.edu.co>
2022-06-01 17:39:19 -0500

    Recepcion de parametros

--- src/main/java/disquera/disquerahm/controllers/generoController.java
+++ src/main/java/disquera/disquerahm/controllers/generoController.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import disquera.disquerahm.models.Genero;
 
 @Controller
+@RequestMapping("/genero")
 public class generoController {
-    @RequestMapping(value = "/genero", method = RequestMethod.GET)
+    @RequestMapping(value = {"/listar","","/"}, method = RequestMethod.GET)
     public ModelAndView verG(ModelAndView mv){
         List<Genero> genero = new ArrayList<>();
         genero.add(new Genero(1,"Pop",true));
@@ -20,5 +22,12 @@ public class generoController {
         mv.addObject("genero", genero);
         mv.setViewName("genero/genero");
         return mv;
-    } 
+    }
+    @RequestMapping(value = "/ver/{id}", method = RequestMethod.GET)
+    public ModelAndView ver(@PathVariable int id,String msn, ModelAndView mv){
+        mv.addObject("id", id+"lol"+id);
+        mv.addObject("msn", msn);
+        mv.setViewName("genero/ver");
+        return mv;
+    }
 }
